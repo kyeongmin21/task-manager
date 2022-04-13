30704861dfc52bb63e66f2bb8c27f02bd4583f7f
kim kyeong min <kyeongmin21@naver.com>
2022-04-13 22:43:40 +0900

    env file 만들기 / mongoose 설치

--- back/app.js
+++ back/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
+import mongoose from "mongoose";
 
 const app = express();
-
-app.listen(3000, () => console.log('listening to port 3000'));
+mongoose.connect('mongodb+srv://min:<password>@cluster0.ahzic.mongodb.net/taskmanager?retryWrites=true&w=majority').then(()=> {
+    app.listen(3000, () => console.log('listening to port 3000'));
+}).catch(err => console.log(err));
