ec5ddb4bc48ebccbe677d0e1eba3731924ba8cfe
kim kyeong min <kyeongmin21@naver.com>
2022-04-13 23:24:30 +0900

    DB_USERNAME 수정

--- back/app.js
+++ back/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
-mongoose.connect('mongodb+srv://min:<password>@cluster0.ahzic.mongodb.net/taskmanager?retryWrites=true&w=majority').then(()=> {
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ahzic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`).then(()=> {
     app.listen(3000, () => console.log('listening to port 3000'));
 }).catch(err => console.log(err));
